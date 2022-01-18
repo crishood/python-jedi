8afe3ab599f609e44623bf25e85b537363a3d9a0
CrisHood <root@Aura.localdomain>
2022-01-18 11:18:57 -0500

    Comprehensions & Lambdas training

--- debugging.py
+++ debugging.py
@@ -1,14 +1,22 @@
 def divisors(num):
     divisors = []
     for i in range(1, num + 1):
-        if num % i == 1:
+        if num % i == 0:
             divisors.append(i)
-        return divisors
+    return divisors
+
 
 def run():
-    num = int(input('Ingresa un número plis: '))
-    print(divisors(num))
-    print('Final')
+    while True:
+        try:
+            num = int(input('Ingresa un número: '))
+            if num < 0:
+                raise ValueError
+            print(divisors(num))
+            print("Terminó mi programa")
+            break
+        except ValueError:
+            print("Debes ingresar un entero positivo")
 
 
 if __name__ == '__main__':
